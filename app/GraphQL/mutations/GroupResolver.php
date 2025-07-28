16a37636b3ed024be31e89af4e9a2c6b603bb3aa
Tôm <bthvuong23@gmail.com>
2025-07-28 09:31:51 +0700

    fix: editGroup

--- app/GraphQL/mutations/GroupResolver.php
+++ app/GraphQL/mutations/GroupResolver.php
@@ -5,6 +5,8 @@ namespace App\GraphQL\Mutations;
 use App\Services\GroupService;
 use GraphQL\Type\Definition\ResolveInfo;
 use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
+use Illuminate\Support\Facades\Log;
+use App\Models\Group;
 
 class GroupResolver
 {
@@ -15,9 +17,20 @@ class GroupResolver
         $this->groupService = $groupService;
     }
 
-    public function updateGroupById($_, array $args, GraphQLContext $context, ResolveInfo $resolveInfo)
+    public function updateGroupById($_, array $args)
     {
-        return $this->groupService->editGroup($args['input']);
+        $input = $args['input'];
+        $group = Group::findOrFail($input['id']);
+
+        $group->update([
+            'name' => $input['name'] ?? $group->name,
+            'type_id' => $input['type_id'] ?? $group->type_id,
+            'version_id' => $input['version_id'] ?? $group->version_id,
+            'assembler_id' => $input['assembler_id'] ?? $group->assembler_id,
+            'is_optional' => $input['is_optional'] ?? $group->is_optional,
+        ]);
+
+        return $group;
     }
 
     public function createGroup($_, array $args)
